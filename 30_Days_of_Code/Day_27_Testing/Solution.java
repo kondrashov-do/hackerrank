9b7e1313f332fea3f634a48efb7a75ac89f1fb81
Dima89 <kondrashov.do@gmail.com>
2017-10-14 11:59:47 +0200

    Day 27/30, Testign solved

---
+++ 30_Days_of_Code/Day_27_Testing/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+
+
+        System.out.println("4 3");
+        System.out.println("-1 -3 4 2");
+
+
+        System.out.println("5 2");
+        System.out.println("0 -1 2 1 4");
+
+        System.out.println("4 3");
+        System.out.println("-1 -3 4 2");
+
+
+        System.out.println("4 3");
+        System.out.println("1 3 4 2");
+
+
+        System.out.println("4 3");
+        System.out.println("0 0 4 2");
+
+    }
+}
