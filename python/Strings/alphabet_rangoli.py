355545fe0953e121b36ca52d6676ec10517ee5df
Dima89 <kondrashov.do@gmail.com>
2018-02-11 14:24:57 +0100

    alphabet_rangoli.py

---
+++ python/Strings/alphabet_rangoli.py
@@ -0,0 +1,10 @@
+def print_rangoli(size):
+    import string
+    alpha = string.ascii_lowercase
+    L =[]
+    for i in range(size):
+        s = '-'.join(alpha[i:size])
+        L.append((s[::-1]+s[1:]).center(4*size-3,'-'))
+    print('\n'.join(L[size-1:0:-1]+L))
+n = int(input())
+print_rangoli(n)
