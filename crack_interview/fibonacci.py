9e8fb0321b7bc205c4320a4f4728d61caedd6cf9
Dima K <kondrashov.do@gmail.com>
2018-03-30 12:51:33 +0200

    fibonacci solved

---
+++ crack_interview/fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    # Write your code here.
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+n = int(input())
+print(fibonacci(n))
